e68a4276a1e1338d6ac7892e93140c31e8205a57
gue02 <kyu6716@naver.com>
2026-09-15 00:46:55 +0900

    third

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI,Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/img", StaticFiles(directory="static/img"),name="img")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/{username}")
-def read_root(request: Request, username : str):
-    # 2026.05.28 업데이트: Starlette 1.0 호환을 위해 request를 첫 번째 인자로 전달합니다.
-    return templates.TemplateResponse(request, "index.html", {"username": username})
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse(request, "index.html")
